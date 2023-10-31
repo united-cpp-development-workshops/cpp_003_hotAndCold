f6a2efbeee13566660c040d089a5b314279a9381
EmrecanKaracayir <developer@karacayir.com>
2023-10-31 21:44:09 +0300

    Initial Commit

---
+++ cpp_003_hotAndCold/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_003_hotAndCold
+//
+//  Created by Emrecan Karaçayır on 31/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
